5e42196c4c3cfca83f8d46fb0b8131b347d8476e
aldimhernandez <marialdanahz@gmail.com>
2025-04-06 11:12:11 -0300

    fix: :art: update imports that was configurated from webdriverio wizard

--- test/pages/Secure.page.ts
+++ test/pages/Secure.page.ts
@@ -1,5 +1,5 @@
 import { $ } from '@wdio/globals'
-import Page from './page.js';
+import Page from './page';
 
 /**
  * sub page containing specific selectors and methods for a specific page
@@ -8,7 +8,7 @@ class SecurePage extends Page {
     /**
      * define selectors using getter methods
      */
-    public get flashAlert () {
+    public get flashAlert() {
         return $('#flash');
     }
 }
--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,6 +1,6 @@
-import { expect } from '@wdio/globals'
-import LoginPage from '../pageobjects/login.page.js'
-import SecurePage from '../pageobjects/secure.page.js'
+import { expect } from '@wdio/globals';
+import LoginPage from '../pages/login.page';
+import SecurePage from '../pages/secure.page';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
